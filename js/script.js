b0f05fe6d8e3b38bc2bd2da4f815197371609d99
Montesi-Yuri <yuri13312@gmail.com>
2023-06-23 15:58:49 +0200

    ADD - started setTimeout function

--- js/script.js
+++ js/script.js
@@ -23,25 +23,33 @@
 const firstNum = randomNumber(1,10);
 console.log('firstNum', firstNum, typeof firstNum);
 const firstCont = document.getElementById('first');
+firstCont.innerHTML = firstNum;
 
 const secondNum = randomNumber(1,10);
 console.log('secondNum', secondNum, typeof secondNum);
 const secondCont = document.getElementById('second');
+secondCont.innerHTML = secondNum;
 
 const thirdNum = randomNumber(1,10);
 console.log('thirdNum', thirdNum, typeof thirdNum);
 const thirdCont = document.getElementById('third');
+thirdCont.innerHTML = thirdNum;
 
 const fourthNum = randomNumber(1,10);
 console.log('fourthNum', fourthNum, typeof fourthNum);
 const fourthCont = document.getElementById('fourth');
+fourthCont.innerHTML = fourthNum;
 
 const fifthNum = randomNumber(1,10);
 console.log('fifthNum', fifthNum, typeof fifthNum);
 const fifthCont = document.getElementById('fifth');
+fifthCont.innerHTML = fifthNum;
 
+const randomNumbers = [firstNum, secondNum, thirdNum, fourthNum, fifthNum];
 
+console.log('numeri randomici', randomNumbers);
 
+const numbersCont = [firstCont, secondCont, thirdCont, fourthCont, fifthCont];
 
 
 
@@ -56,3 +64,39 @@ const fifthCont = document.getElementById('fifth');
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+
+setTimeout(
+    function(){
+        
+        for (let i = 0; i < randomNumbers.length; i++) {
+
+            console.log(randomNumbers[i]);
+            numbersCont[i].classList.add('d-none');
+        }
+
+        let userFirstNum = parseInt(prompt('Inserisci il primo numero che hai visto'));
+        let userSecondNum = parseInt(prompt('Inserisci il secondo numero che hai visto'));
+        let userThirdNum = parseInt(prompt('Inserisci il terzo numero che hai visto'));
+        let userFourthNum = parseInt(prompt('Inserisci il quarto numero che hai visto'));
+        let userFifthNum = parseInt(prompt('Inserisci il quinto numero che hai visto'));
+
+        console.log('userFirstNum', userFirstNum);
+        console.log('userSecondNum', userSecondNum);
+        console.log('userThirdNum', userThirdNum);
+        console.log('userFourthNum', userFourthNum);
+        console.log('userFifthNum', userFifthNum);
+
+        const userNumbers = [userFirstNum, userSecondNum, userThirdNum, userFourthNum, userFifthNum];
+        console.log('Numeri inseriti dall utente', userNumbers)
+
+        for (let i = 0; i < randomNumbers.length; i++) {
+            
+            if (randomNumbers[i] == userNumbers[i]){
+                userNumbers[i].classList.add('')
+            }
+        }
+    
+        
+        
+
+    }, 5000);
