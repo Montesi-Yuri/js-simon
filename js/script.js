48d128b9a390e23cc3d281f58097d9b8dbb53a8f
Montesi-Yuri <yuri13312@gmail.com>
2023-06-23 14:07:41 +0200

    ADD - procedure list JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 /* 
     JS
 */
+
+/*
+    1- Creo funzione per numeri casuali
+    2- Creo 5 variabili con all'interno la funzione per i numeri casuali
+    3- Inserisco le 5 variabili in un array
+    4- Inserisco le 5 variabili nell'html
+    5- SetTimeout per nascondere i 5 numeri dopo 30 secondi
+    6- 5 prompt per permettere all'utente di inserire i numeri nell'ordine corretto
+    7- Inserisco i numeri dell'utente in un array
+    8- Confronto l'array dei numeri casuali con l'array dei numeri dell'utente
+    9- SE le array sono identiche l'utente ha vinto
+        ALTRIMENTI le array non sono identiche l'utente ha perso
+    10- Inserisco le array a confronto nell'html
+        SE in ordine crescente un numero dell'array dell'utente corrisponde all'array dei numeri casuali gli viene data una classe
+        ALRIMENTI gli viene data un altra classe
